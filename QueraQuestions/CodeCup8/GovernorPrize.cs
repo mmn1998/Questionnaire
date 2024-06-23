d9b4f5b48fa4cea23f204976d17fbe76744bee18
mmn1998 <mm.nomani.1998@gmail.com>
2024-06-23 08:54:29 +0330

    .

---
+++ QueraQuestions/CodeCup8/GovernorPrize.cs
@@ -0,0 +1,47 @@
+﻿namespace QueraQuestions.CodeCup8;
+
+public class GovernorPrize
+{
+    private readonly List<GraphNodes> _nodes;
+    public GovernorPrize(List<GraphNodes> nodes)
+    {
+        _nodes = nodes;
+    }
+    public int Compute()
+    {
+        int result = 0;
+        int minimumPrizeForNotRoots = 2;
+        foreach (var node in _nodes)
+        {
+            if (node.IsRoot)
+            {
+                result++;
+                node.Prize = 1;
+            }
+            else
+            {
+                if (node.Neighbors is not null)
+                {
+                    foreach (var neighborId in node.Neighbors)
+                    {
+                        var neighbor = _nodes.First(x => x.Id == neighborId);
+                        if (node.Neighbors.Contains(neighborId) && !neighbor.IsRoot && neighbor.Prize != 0)
+                        {
+                            minimumPrizeForNotRoots = minimumPrizeForNotRoots + 1;
+                        }
+                    }
+                }
+                result = result + minimumPrizeForNotRoots;
+                node.Prize = minimumPrizeForNotRoots;
+            }
+        }
+        return result;
+    }
+}
+public class GraphNodes
+{
+    public int Id { get; set; }
+    public bool IsRoot { get; set; }
+    public int Prize { get; set; } = 0;
+    public List<int>? Neighbors { get; set; }
+}
---
+++ QueraQuestions/Program.cs
@@ -0,0 +1,70 @@
+﻿// See https://aka.ms/new-console-template for more information
+using QueraQuestions.CodeCup8;
+
+//var graph = new List<GraphNodes>
+//{
+//    new GraphNodes
+//    {
+//        Id = 1,
+//        IsRoot = true,
+//        Neighbors = new List<int> { 7 }
+//    },new GraphNodes
+//    {
+//        Id = 2,
+//        IsRoot = true,
+//        Neighbors = new List<int> { 7 }
+//    },new GraphNodes
+//    {
+//        Id = 3,
+//        IsRoot = true,
+//        Neighbors = new List<int> { 7 }
+//    },new GraphNodes
+//    {
+//        Id = 6,
+//        IsRoot = true,
+//        Neighbors = new List<int> { 4 }
+//    },new GraphNodes
+//    {
+//        Id = 5,
+//        IsRoot = true,
+//        Neighbors = new List<int> { 4 }
+//    },new GraphNodes
+//    {
+//        Id = 8,
+//        IsRoot = true,
+//        Neighbors = new List<int> { 4 }
+//    }
+//    ,new GraphNodes
+//    {
+//        Id = 4,
+//        IsRoot = false,
+//        Neighbors = new List<int> { 8,5,6,7 }
+//    },new GraphNodes
+//    {
+//        Id = 7,
+//        IsRoot = false,
+//        Neighbors = new List<int> { 1,2,3,4 }
+//    }
+//};
+//var obj = new GovernorPrize(graph);
+//Console.WriteLine(obj.Compute().ToString());
+//Console.Read();
+
+
+
+
+int totalNodes = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= totalNodes; i++)
+{
+    string? input = Console.ReadLine();
+    if (!string.IsNullOrEmpty(input))
+    {
+        var splitedInput = input.Trim().Split(" ");
+        foreach (var item in splitedInput)
+        {
+
+        }
+        var obj = new GraphNodes { Id = i, IsRoot = false, Neighbors =  };
+    }
+
+}
